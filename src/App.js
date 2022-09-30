32ac29f3065f0a4fb242467f163beda73aae0c61
cloudwi <cloudwi@naver.com>
2022-09-30 15:53:10 +0900

    repaly
    Signed-off-by: cloudwi <cloudwi@naver.com>

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> 배포 성공
+          Edit <code>src/App.js</code> 배포 성공 ??
         </p>
         <a
           className="App-link"
